5b0c5d9029933783a32d1fbb3a78f45651d06d87
Randy Rafael Bermudez Robles <randyrafael0110@gmail.com>
2021-10-28 10:20:04 -0500

    actualizaciones para heroku2

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/prueba-qrvey'));
+app.use(express.static(__dirname+'/dist/PruebaQrvey'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/prueba-qrvey/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/PruebaQrvey/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
